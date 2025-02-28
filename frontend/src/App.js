a414634db8285a3ed2d369e40c5da63f0e3e562e
Vlad <vlad@theletsgoapp.com>
2025-02-28 01:33:11 -0500

    Cursor is an actual cursor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,13 +11,26 @@ const Cursor = ({ x, y, color}) => {
 				position: 'absolute',
 				top: y,
 				left: x,
-				width: '10px',
-				height: '10px',
-				backgroundColor: color,
-				borderRadius: '10%',
-				pointerEvents: 'none'
+				width: '20px',
+				height: '20px',
+				pointerEvents: 'none',
+				transform: 'translate(-50%, -50%)',
 			}}
-		/>
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill={color}
+				stroke="black"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path d="M3 3l6 16 4-7 7-4-17-5z" />
+			</svg>
+		</div>
 	);
 }
 
